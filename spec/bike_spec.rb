ec3b0283166c456f332c8aac068265df3868f790
Enrico Graziani <mrenrich84@gmail.com>
2017-01-07 15:53:38 +0000

    Bike: can report_broken itself

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,14 @@
 require 'bike'
 
 describe Bike do
-  it 'responds to working?' do
-    expect(subject).to respond_to :working?
+  it { is_expected.to respond_to(:working?) }
+
+  context "when set to not working" do
+    before do
+      subject.report_broken
+    end
+    it "report itself as not working" do
+      expect(subject.working?).to eq(false)
+    end
   end
 end
